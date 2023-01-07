7e199c2b6a07cd8ba93e193042fb5fca7d09ea81
JaehoonSim <18tomcat@gmail.com>
2023-01-07 10:16:09 +0900

    123

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import requests
 
 import json
 
-url = 'https://series.naver.com/novel/categoryProductList.series?categoryTypeCode=genre&genreCode=207&orderTypeCode=sale&is&isFinished=false'
+url = 'https://series.naver.com/novel/categoryProductList.series?categoryTypeCode=genre&genreCode=207&orderTypeCode=new&is&isFinished=false'
 req_header_dict = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
 }
